bf4e6c3a560b6404e3af0f9b4540583a9c9007d3
hligy <1970316213@qq.com>
2020-09-10 09:46:10 +0800

    ioc-container

--- ioc-container-overview/src/main/java/com/example/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ ioc-container-overview/src/main/java/com/example/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -36,6 +36,9 @@ public class DependencyInjectionDemo {
     // 依赖来源三: 容器内建 Bean
     Environment environment = applicationContext.getBean(Environment.class);
     System.out.println("获取 Environment 类型的 bean:" + environment);
+
+    // 这个才是真正的容器（beanFactory）
+//    System.out.println(applicationContext.getBeanFactory() == userRepository.getBeanFactory());
   }
 
   private static void whoIsIocContainer(UserRepository userRepository, BeanFactory beanFactory) {
