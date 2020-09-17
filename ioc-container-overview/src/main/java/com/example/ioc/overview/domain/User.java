8b30fbfdc3fd8b073a94514713c2b0bfb8081f62
hligy <1970316213@qq.com>
2020-09-17 18:07:12 +0800

    ioc-container-overview

--- ioc-container-overview/src/main/java/com/example/ioc/overview/domain/User.java
+++ ioc-container-overview/src/main/java/com/example/ioc/overview/domain/User.java
@@ -1,8 +1,28 @@
 package com.example.ioc.overview.domain;
 
+import com.example.ioc.overview.enums.City;
+
 public class User {
   private int id;
   private String name;
+  private City city;
+
+  public City getCity() {
+    return city;
+  }
+
+  public void setCity(City city) {
+    this.city = city;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", city=" + city +
+            '}';
+  }
 
   public int getId() {
     return id;
@@ -18,14 +38,6 @@ public class User {
 
   public void setName(String name) {
     this.name = name;
-  }
-
-  @Override
-  public String toString() {
-    return "User{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            '}';
   }
 
   public static User createMethod() {
---
+++ ioc-container-overview/src/main/java/com/example/ioc/overview/enums/City.java
@@ -0,0 +1,16 @@
+package com.example.ioc.overview.enums;
+
+public enum City {
+  /**
+   * 北京
+   */
+  BEIJING,
+  /**
+   * 杭州
+   */
+  HANGZHOU,
+  /**
+   * 上海
+   */
+  SHANGHAI
+}
