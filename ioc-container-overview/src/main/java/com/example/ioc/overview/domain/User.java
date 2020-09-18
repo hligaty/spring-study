b2d2c44e4d92e12f0e495ca8f2cd7eed466d2cd1
hligy <1970316213@qq.com>
2020-09-18 10:17:20 +0800

    ioc-container-overview

--- ioc-container-overview/src/main/java/com/example/ioc/overview/domain/User.java
+++ ioc-container-overview/src/main/java/com/example/ioc/overview/domain/User.java
@@ -3,12 +3,45 @@ package com.example.ioc.overview.domain;
 import com.example.ioc.overview.enums.City;
 import org.springframework.core.io.Resource;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 public class User {
   private int id;
   private String name;
   private City city;
   private Resource configFileLocation;
+  private City[] workCities;
+  private List<City> lifeCities;
+
+  @Override
+  public String toString() {
+    return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", city=" + city +
+            ", configFileLocation=" + configFileLocation +
+            ", workCities=" + Arrays.toString(workCities) +
+            ", lifeCities=" + lifeCities +
+            '}';
+  }
+
+  public List<City> getLifeCities() {
+    return lifeCities;
+  }
+
+  public void setLifeCities(List<City> lifeCities) {
+    this.lifeCities = lifeCities;
+  }
+
+  public City[] getWorkCities() {
+    return workCities;
+  }
+
+  public void setWorkCities(City[] workCities) {
+    this.workCities = workCities;
+  }
 
   public Resource getConfigFileLocation() {
     return configFileLocation;
@@ -24,16 +57,6 @@ public class User {
 
   public void setCity(City city) {
     this.city = city;
-  }
-
-  @Override
-  public String toString() {
-    return "User{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            ", city=" + city +
-            ", resource=" + configFileLocation +
-            '}';
   }
 
   public int getId() {
